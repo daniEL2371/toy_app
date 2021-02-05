b0d0c2203e0e6ca276fd6c4b1e2d1d046f193bdc
Daniel <danielzelalemheru@gmail.com>
2021-02-05 17:26:48 +0300

    microposts added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_05_141916) do
+ActiveRecord::Schema.define(version: 2021_02_05_142511) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
