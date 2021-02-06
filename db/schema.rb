93d7cf1410ae51010278809bf74e570daef0e918
Daniel <danielzelalemheru@gmail.com>
2021-02-06 09:59:02 +0300

    Micropost model updated to contain user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_05_142511) do
+ActiveRecord::Schema.define(version: 2021_02_06_065729) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
